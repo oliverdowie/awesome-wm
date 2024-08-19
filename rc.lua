0bef72b61df3ef30802bf829a0861f2bc678248f
oliverdowie <oliver_dowie@hotmail.com>
2024-08-19 17:35:47 +0930

    Fixed layout icon size and position

--- rc.lua
+++ rc.lua
@@ -69,19 +69,19 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.floating,
     awful.layout.suit.tile,
+    awful.layout.suit.floating,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
     awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    --awful.layout.suit.max,
-    --awful.layout.suit.max.fullscreen,
-    --awful.layout.suit.magnifier,
-    awful.layout.suit.corner.nw,
+    -- awful.layout.suit.spiral.dwindle,
+    -- awful.layout.suit.max,
+    -- awful.layout.suit.max.fullscreen,
+    -- awful.layout.suit.magnifier,
+    -- awful.layout.suit.corner.nw,
     -- awful.layout.suit.corner.ne,
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
@@ -187,24 +187,37 @@ awful.screen.connect_for_each_screen(function(s)
     -- Each screen has its own tag table.
     local names = { "hack", "www", "chats", "beats", "5", "6", "7", "8", "9" }
     local l = awful.layout.suit  -- Just to save some typing: use an alias.
-    local layouts = { l.tile, l.tile, l.tile, l.tile, l.floating,
-    l.floating, l.floating, l.floating, l.floating }
+    local layouts = { l.tile, l.tile, l.tile, l.tile, l.tile,
+    l.tile, l.tile, l.tile, l.tile }
     awful.tag(names, s, layouts)
 
     s.mypromptbox = awful.widget.prompt()
     -- Create an imagebox widget which will contain an icon indicating which layout we're using.
     -- We need one layoutbox per screen.
-    s.mylayoutbox = awful.widget.layoutbox(s)
+    s.mylayoutbox = awful.widget.layoutbox({
+        screen = s,
+        forced_height = 20,
+        forced_width = 20
+    })
     s.mylayoutbox:buttons(gears.table.join(
                            awful.button({ }, 1, function () awful.layout.inc( 1) end),
                            awful.button({ }, 3, function () awful.layout.inc(-1) end),
                            awful.button({ }, 4, function () awful.layout.inc( 1) end),
                            awful.button({ }, 5, function () awful.layout.inc(-1) end)))
-    -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist {
-        screen  = s,
-        filter  = awful.widget.taglist.filter.all,
-        buttons = taglist_buttons
+    
+    -- Rotate and resize layout icon
+    local layout_icon = wibox.widget {
+        widget = wibox.container.rotate,
+        direction = 'east',
+        wibox.widget {
+            widget = wibox.container.margin,
+            margins = {
+                left = 10,
+                bottom = 10,
+                top = 10
+            },
+            s.mylayoutbox
+        }
     }
 
     -- Create a tasklist widget
@@ -248,7 +261,7 @@ awful.screen.connect_for_each_screen(function(s)
                 -- mykeyboardlayout,
                 wibox.widget.systray(),
                 custom_clock,
-                s.mylayoutbox,
+                layout_icon,
             },
         }
     }
